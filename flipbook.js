1c3ddf7944e26a99e27e151b795184102d63ee58
John Goodman <johncurtisgoodman@gmail.com>
2018-06-27 17:28:52 -0400

    Write first draft of flipbook script.

--- flipbook.js
+++ flipbook.js
@@ -1,42 +1,54 @@
-let Flipbook = function (imageSource, frameCount, container) {
-    this.absoluteImage = document.createElement('img')
-    this.absoluteImage.src = imageSource
-    this.image = document.createElement('div')
-    this.image.style.backgroundImage = `url('${imageSource}')`
-    this.index = 0
-    this.scale = 1
-    this.frameCount = frameCount
-    container.appendChild(this.image)
-    this.absoluteImage.addEventListener('load', () => {
-        this.image.style.width = `${this.absoluteImage.width * this.scale / frameCount}px`
-        this.image.style.height = `${this.absoluteImage.width * this.scale / frameCount}px`
-        this.image.style.backgroundSize = `${this.absoluteImage.width * this.scale}px ${this.absoluteImage.height * this.scale}px`
-        // window.setInterval(() => {
-        //     this.index += 1
-        //     this.update()
-        // }, 70)
+let canvas = null
+let ctx = null
+let frameRate = 100
+
+let setupCanvas = () => {
+    canvas = document.getElementById('canvas')
+    ctx = canvas.getContext('2d')
+    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+    drawPanels()
+}
+
+function Panel (obj) {
+    this.frames = obj.frames
+    this.frame = 0
+    this.image = document.createElement('img')
+    this.image.src = obj.imageSource
+}
+
+let drawPanels = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    let objectiveScroll = window.scrollY
+    panels.map((panel, index) => {
+        let anchor = height(panel, canvas) * index
+        let slide = panel.frames * frameRate
+        if (window.scrollY > anchor && window.scrollY < anchor + slide) {
+            objectiveScroll = anchor
+            panel.frame = Math.floor(window.scrollY / (anchor + slide) * panel.frames)
+        } else if (window.scrollY > anchor + slide) {
+            objectiveScroll -= slide
+        }
+        let scrollOffset = anchor - objectiveScroll
+        let frameOffset = panel.frame * canvas.width
+        ctx.drawImage(panel.image, 0 - frameOffset, scrollOffset, canvas.width * panel.frames, height(panel, canvas))
     })
 }
 
-Flipbook.prototype.update = function () {
-    this.image.style.backgroundPositionX = `${
-        (-1) * this.absoluteImage.width * this.scale / this.frameCount * this.index
-    }px`
+let height = (panel, canvas) => {
+    return (panel.image.width / panel.frames * canvas.width) / panel.image.height
 }
 
-window.addEventListener('load', () => {
-    let container = document.getElementById('flipbook-container')
-    window.demo = new Flipbook ('images/deeps-demo-filmstrip.png', 7, container)
-})
+let panels = [
+    new Panel ({
+        imageSource: 'images/deeps-demo-color.png',
+        frames: 7,
+    }),
+    new Panel ({
+        imageSource: 'images/deeps-demo-filmstrip.png',
+        frames: 7,
+    }),
+]
 
-let onScroll = () => {
-    let minDiff = 0
-    let maxDiff = document.body.scrollHeight - (demo.image.getBoundingClientRect().height * 2)
-    demo.index = Math.round((
-        (demo.image.offsetTop - demo.image.getBoundingClientRect().top) / maxDiff
-    ) * demo.frameCount)
-    demo.update()
-}
-
-window.addEventListener('scroll', onScroll)
-window.addEventListener('touchmove', onScroll)
+window.addEventListener('load', setupCanvas)
+window.addEventListener('scroll', drawPanels)
