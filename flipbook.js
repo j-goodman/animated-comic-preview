84265132434905f0eb76a6633c113748fc97f7b5
John Goodman <johncurtisgoodman@gmail.com>
2018-06-28 13:41:39 -0400

    Fix panel anchoring bug.

--- flipbook.js
+++ flipbook.js
@@ -13,6 +13,7 @@ let setupCanvas = () => {
 function Panel (obj) {
     this.frames = obj.frames
     this.frame = 0
+    this.name = obj.name
     this.image = document.createElement('img')
     this.image.src = obj.imageSource
 }
@@ -20,17 +21,17 @@ function Panel (obj) {
 let drawPanels = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     let objectiveScroll = window.scrollY
-    let totalSlide = 0
+    let totalFrames = 0
     panels.map((panel, index) => {
         let anchor = height(panel, canvas) * index
         let slide = panel.frames * frameRate
-        if (objectiveScroll > anchor && window.scrollY < anchor + slide) {
+        if (objectiveScroll > anchor && objectiveScroll < anchor + slide) {
             objectiveScroll = anchor
-            panel.frame = Math.floor(window.scrollY / (anchor + slide) * panel.frames)
+            panel.frame = Math.floor((window.scrollY - anchor) / (slide) * panel.frames - totalFrames)
         } else if (objectiveScroll > anchor + slide) {
             objectiveScroll -= slide
-            totalSlide += slide
         }
+        totalFrames += panel.frames
         let scrollOffset = anchor - objectiveScroll
         let frameOffset = panel.frame * canvas.width
         ctx.drawImage(panel.image, 0 - frameOffset, scrollOffset, canvas.width * panel.frames, height(panel, canvas))
@@ -38,16 +39,28 @@ let drawPanels = () => {
 }
 
 let height = (panel, canvas) => {
-    return (panel.image.width / panel.frames * canvas.width) / panel.image.height
+    return (panel.image.width / panel.frames * window.innerWidth) / panel.image.height
 }
 
 let panels = [
     new Panel ({
+        name: 'color-1',
         imageSource: 'images/deeps-demo-color.png',
         frames: 7,
     }),
     new Panel ({
-        imageSource: 'images/deeps-demo-filmstrip.png',
+        name: 'line-1',
+        imageSource: 'images/deeps-demo-lineart.png',
+        frames: 7,
+    }),
+    new Panel ({
+        name: 'color-2',
+        imageSource: 'images/deeps-demo-color.png',
+        frames: 7,
+    }),
+    new Panel ({
+        name: 'line-2',
+        imageSource: 'images/deeps-demo-lineart.png',
         frames: 7,
     }),
 ]
